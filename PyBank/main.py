dbb50ed340d3db9562d66fd8c27b0752b08c6dba
Smakdata24 <Makoshisj2014@gmail.com>
2021-01-21 23:03:18 -0600

    Final code of pybank

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,7 +4,8 @@ import csv
 budget_data = os.path.join("Resources", "budget_data.csv")
 
 months = []
-net_total = []
+total_profit = []
+monthly_profit_change = []
 
 
 
@@ -16,30 +17,31 @@ with open(budget_data, newline='') as csvfile:
 
 
     for row in csvreader:
-        print(row)
+
 
         months.append(row[0])
-        net_total.append(int(row[1]))
-
-
-total_months = len(months)
-
-total_change = sum(net_total)
+        total_profit.append(int(row[1]))
 
 
 
-
-Total_average = []
-Total_average = sum(total_change) / len(total_change)
-
-for i in range(len(net_total)-1):
-    total_average.append(net_total[i+1]-net_total[i])
+    for i in range(len(total_profit)-1):
+        monthly_profit_change.append(total_profit[i+1]-total_profit[i])
 
 
 
 
-greatest_increase = max(total_change)
-greatest_decrease = min(total_change)
+
+greatest_increase = max(monthly_profit_change)
+greatest_decrease = min(monthly_profit_change)
+
+
+
+max_increase_month = monthly_profit_change.index(max(monthly_profit_change)) + 1
+max_decrease_month = monthly_profit_change.index(min(monthly_profit_change)) + 1 
+
+
+
+
 
 
      
@@ -47,11 +49,52 @@ greatest_decrease = min(total_change)
 
 print('Financial Analysis')
 print('-----------------------------')
-print("Total Months: " + str(total_months))
-print("Total: " + "$" + str(sum(total_change)))
-print("Average Change: " + "$" + str(total_average))
-print("Greatest Increase in Profits: " + "$" + str(months[total_change.index(max(total_change))+1]) + " " + "$" + str(greatest_increase))
-print("Greatest Decrease in Profits: " + "$" + str(months[total_change.index(max(total_change))+1]) + " " + "$" + str(greatest_decrease))
+print(f"Total Months: {len(months)}")
+print(f"Total: + ${sum(total_profit)}")
+print(f"Average Change: {round(sum(monthly_profit_change)/len(monthly_profit_change),2)}")
+print(f"Greatest Increase in Profits: {months[max_increase_month]} (${(str(greatest_increase))})")
+print(f"Greatest Decrease in Profits: {months[max_decrease_month]} (${(str(greatest_decrease))})")
+
+
+
+
+
+
+text_file = path("python-challenge", "PyBank", "Analysis.txt")
+
+with open(text_file, "W") as file
+
+
+
+file.write("Analysis")
+    file.write("\n")
+    file.write("----------------------------")
+    file.write("\n")
+    file.write(f"Total Months: {len(months)}")
+    file.write("\n")
+    file.write(f"Total: + ${sum(total_profit)}")
+    file.write("\n")
+    file.write(f"Average Change: {round(sum(monthly_profit_change)/len(monthly_profit_change),2)}")
+    file.write("\n")
+    file.write(f"Greatest Increase in Profits: {months[max_increase_month]} (${(str(greatest_increase))})")
+    file.write("\n")
+    file.write(f"Greatest Decrease in Profits: {months[max_decrease_month]} (${(str(greatest_decrease))})")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
